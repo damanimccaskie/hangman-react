6af188975572d34c0cab14fc190b05f24f95ead9
TheNotoriousMac <damani.mccaskie@mycavehill.uwi.edu>
2022-02-06 11:34:17 -0400

    Hangman game created with React.

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+  <>
+    <h1>Hangman</h1>
+    <p>Find the hidden word - Enter a letter</p>
+
+  </>
+  )
+};
+
+export default Header;
+
